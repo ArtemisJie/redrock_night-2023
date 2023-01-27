3cf570157d3aa2b843959a8b6a706ea716ead806
fuummi <709097694@qq.com>
2023-01-28 02:26:56 +0800

    feat: first page layout complete

---
+++ src/pages/introduction/components/summary/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './index.less'
+
+export default function index() {
+  return (
+    <div>
+      概况
+    </div>
+  )
+}
---
+++ src/pages/main_page/index.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import Content from './components/content'
+import './index.less'
+
+export default function index() {
+    const tabNames = ['开幕式', '圆桌第一期', '圆桌第二期', '敬请期待']
+    const [nowContent, setNowContent] = useState(0)
+
+    function changeContent(index: number) {
+        return () => {
+            setNowContent(index)
+        }
+    }
+    return (
+        <div className='main_root'>
+            <div className='poster'></div>
+            <ul className='switch'>
+                {
+                    tabNames.map((e, i) => <li key={i} onClick={changeContent(i)}>{e}</li>)
+                }
+            </ul>
+            <Content dataIndex={nowContent}></Content>
+        </div>
+    )
+}
